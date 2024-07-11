9744f16b36ab78045a7c6ab2842f5128b81bfb53
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2024-07-11 13:50:30 +0200

    fix legacy boostrap typo

--- src/Manager/MigrateGlobalColsManager.php
+++ src/Manager/MigrateGlobalColsManager.php
@@ -62,6 +62,7 @@ class MigrateGlobalColsManager extends AbstractMigrationManager
                 }
 
                 $idSource = $profileName === 'bootstrap' ? 'bootstrap3' : $profileName;
+                $idSource = \str_replace('boostrap', 'bootstrap', $idSource);
                 $identifier = \sprintf('globals.%s.%s', $idSource, $setName);
                 $maxColCount = \max(\array_map('count', $breakpoints) ?: [0]);
                 $rowClasses = "colcount_$maxColCount $idSource sc-type-$setName" . (\is_numeric($setName) ? " col_$setName" : " col-$setName");
